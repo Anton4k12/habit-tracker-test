d2bab462d669f247487e7fae9b5d0fd454f7e593
Anton4k12 <neanton2008@gmail.com>
2024-11-01 18:25:16 +0200

    feat: put all in state

--- src/App.jsx
+++ src/App.jsx
@@ -35,25 +35,34 @@ const days = [
 ];
 
 const colors = [
-  "#474B4E",
-  "#015D52",
-  "#826C34",
-  "#CC0605",
-  "#D0D0D0",
-  "#6C6960",
-  "#8F8F8F",
-  "#A52019",
-  "#A65E2E",
-  "#343B29",
+  { id: "Squirrel grey", color: "#78858B" },
+  { id: "Signal blue", color: "#1E2460" },
+  { id: "Pale green", color: "#89AC76" },
+  { id: "Umbra grey", color: "#332F2C" },
+  { id: "Silk grey", color: "#CAC4B0" },
+  { id: "Beige red", color: "#C1876B" },
+  { id: "Purple violet", color: "#4A192C" },
+  { id: "Leaf green", color: "#2D572C" },
+  { id: "Red violet", color: "#922B3E" },
+  { id: "Curry", color: "#9D9101" },
 ];
 
 export const App = () => {
   const [inputValue, setInputValue] = useState("");
   const [durationValue, setDurationValue] = useState("");
   const [selectedColor, setSelectedColor] = useState("");
+  const [selectedDay, setSelectedDay] = useState("");
+  const [timesValue, setTimesValue] = useState("");
+
+  const allInfo =
+    inputValue + timesValue + selectedDay + durationValue + selectedColor;
 
   const handleAlert = () => {
-    alert(inputValue);
+    alert(allInfo);
+  };
+
+  const handleChosenDay = (day) => {
+    setSelectedDay(day);
   };
 
   return (
@@ -69,7 +78,7 @@ export const App = () => {
       </div>
 
       <div className="py-2">
-        <RadioGroup>
+        <RadioGroup value={timesValue} onValueChange={setTimesValue}>
           <div className="flex items-center space-x-2">
             <RadioGroupItem value="everyday" id="everyday" />
             <Label htmlFor="everyday">Everyday</Label>
@@ -84,7 +93,14 @@ export const App = () => {
       <div className="flex gap-2">
         {days.map((day) => {
           return (
-            <div className="cursor-pointer rounded-full border p-2 hover:opacity-60">
+            <div
+              style={{
+                border:
+                  day.id === selectedDay ? "1px solid #84cc16" : undefined,
+              }}
+              onClick={() => handleChosenDay(day.id)}
+              className="cursor-pointer rounded-full border p-2 hover:opacity-60"
+            >
               <div className="p-1">{day.name}</div>
             </div>
           );
@@ -122,11 +138,11 @@ export const App = () => {
         {colors.map((color) => {
           return (
             <div
-              onClick={() => setSelectedColor(color)}
+              onClick={() => setSelectedColor(color.id)}
               style={{
-                backgroundColor: color,
+                backgroundColor: color.color,
                 border:
-                  color === selectedColor ? "1px solid #84cc16" : undefined,
+                  color.id === selectedColor ? "1px solid #84cc16" : undefined,
               }}
               className="cursor-pointer rounded-full border p-4 hover:opacity-85"
             ></div>
